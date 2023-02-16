3393149ca3f483861999db8f5713a506c19f4603
Juan Pablo Sanabria Moscoso <jpsanabria37@soy.sena.edu.co>
2023-02-16 12:28:10 -0500

    Implement authentication service

--- Identity/Services/AccountService.cs
+++ Identity/Services/AccountService.cs
@@ -1,6 +1,9 @@
 ﻿using Application.DTOs.Users;
 using Application.Interfaces;
 using Application.Wrappers;
+using Domain.Settings;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +14,29 @@ namespace Identity.Services
 {
     public class AccountService : IAccountService
     {
-        public Task<Response<AuthenticationResponse>> AuthenticateAsync(AuthenticationRequest request, string ipAddress)
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly JWTSettings jwtSettings;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IDateTimeService dateTimeService;
+
+        public AccountService(UserManager<ApplicationUser> userManager, JWTSettings jwtSettings, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager, IDateTimeService dateTimeService)
         {
-         
+            this.userManager = userManager;
+            this.jwtSettings = jwtSettings;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+            this.dateTimeService = dateTimeService;
         }
 
-        public Task<Response<string>> RegisterAsync(RegisterRequest request, string origin)
+        public Task<Response<AuthenticationResponse>> AuthenticateAsync(AuthenticationRequest request, string ipAddress)
         {
-            
+            throw new NotImplementedException();
+        }
+
+        public async Task<Response<string>> RegisterAsync(RegisterRequest request, string origin)
+        {
+            throw new NotImplementedException();
         }
     }
 }
