3e7b6362634e59dd156a34aa680821ba4428737b
julianforero19 <julian.forero282@misena.edu.co>
2023-03-22 16:29:32 -0500

    sena terminar

--- Application/DTOs/ServicioDto.cs
+++ Application/DTOs/ServicioDto.cs
@@ -11,5 +11,6 @@ namespace Application.DTOs
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string? Descripcion { get; set; }
+        public double Precio { get; set; }
     }
 }
