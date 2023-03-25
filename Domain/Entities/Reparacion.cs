3ce5cf7f99241f2037b41faeab712fae1ba1f140
julianforero19 <julian.forero282@soy.sena.edu.co>
2023-03-25 14:15:31 -0500

    Nuevas tablas

---
+++ Domain/Entities/Reparacion.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Reparacion :AuditableBaseEntity
+    {
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public string Comentarios { get; set; }
+        public string Estado { get; set;}
+        public int VehiculoId { get; set; }
+        public virtual Vehiculo Vehiculo { get; set; }
+
+    }
+}
---
+++ Domain/Entities/ReparacionServicio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ReparacionServicio 
+    {
+        public int ServicioId { get; set; }
+        public virtual Servicio Servicio { get; set; }
+        public int ReparacionId { get; set; }
+        public virtual Reparacion Reparacion { get; set; }
+        public double Precio { get; set; }
+    }
+}
